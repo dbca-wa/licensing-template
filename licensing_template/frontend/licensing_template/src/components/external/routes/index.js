31983ab5a8eafe36aa5a588565ff979091ed7d99
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-05-24 16:14:11 +0800

    Remove project specific front end code

--- licensing_template/frontend/licensing_template/src/components/external/routes/index.js
+++ licensing_template/frontend/licensing_template/src/components/external/routes/index.js
@@ -1,16 +1,5 @@
 import { RouterView } from 'vue-router'
 import ExternalDashboard from '@/components/external/dashboard.vue'
-import Proposal from '@/components/external/proposal.vue'
-import ProposalApply from '@/components/external/proposal_apply.vue'
-import ProposalSubmit from '@/components/external/proposal_submit.vue'
-import Organisation from '@/components/external/organisations/manage.vue'
-import Compliance from '../compliances/access.vue'
-import ComplianceSubmit from '../compliances/submit.vue'
-/*
-import Compliance from '../compliances/access.vue'
-import ComplianceSubmit from '../compliances/submit.vue'
-import Approval from '../approvals/approval.vue'
-*/
 export default
 {
     path: '/external',
@@ -21,34 +10,6 @@ export default
             path: '/external',
             component: ExternalDashboard,
             name: 'external-dashboard'
-        },
-        {
-            path: 'organisations/manage/:org_id',
-            component: Organisation
-        },
-        {
-            path: 'compliance/:compliance_id',
-            component: Compliance
-        },
-        {
-            path: 'compliance/submit',
-            component: ComplianceSubmit,
-            name:"submit_compliance"
-        },
-        {
-            path: 'proposal/',
-            component: ProposalApply,
-            name:"apply_proposal"
-        },
-        {
-            path: 'proposal/submit',
-            component: ProposalSubmit,
-            name:"submit-proposal"
-        },
-        {
-            path: 'proposal/:proposal_id',
-            component: Proposal,
-            name:"draft_proposal"
         },
     ]
 }
