e99326613beec8147acbf9ea00ee8771040774b1
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-07 16:53:48 +0800

    Add rename_project.sh

---
+++ rename_project.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+if [[ $# -lt 1 ]]; then
+    echo "Missing script parameter:  you must supply the system name to replace 'licensing_template'"
+    echo "$0 1"
+    exit 1
+fi
+
+# rename dirs
+mv ./licensing_template/ ./$1/ &&
+DIR_LIST=$(find . -type d -name "*licensing_template*")
+for f in $DIR_LIST
+do 
+    mv $f $(echo $f | sed "s/licensing_template/$1/g")
+done &&
+
+# rename files 
+FILE_LIST=$(find . -type f -name "*licensing_template*")
+for f in $FILE_LIST
+do 
+    mv $f $(echo $f | sed "s/licensing_template/$1/g")
+done &&
+
+# replace all occurrences of "licensing_system" with new system name
+find . -type f -exec sed -i "s/licensing_template/$1/g" {} \;
