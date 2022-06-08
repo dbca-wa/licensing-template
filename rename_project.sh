3084c3669b071707c352a7734b902df69ab2d675
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-08 12:12:45 +0800

    Small bugfixes

--- rename_project.sh
+++ rename_project.sh
@@ -29,26 +29,26 @@ gitdir=${gitrepo/"-"/"_"}
 cd licensing-template &&
 
 # rename dirs
-mv ./licensing_template/ ./gitdir/ &&
+mv ./licensing_template/ ./$gitdir/ &&
 DIR_LIST=$(find . -type d -name "*licensing_template*")
 for f in $DIR_LIST
 do 
-    mv $f $(echo $f | sed "s/licensing_template/gitdir/g")
+    mv $f $(echo $f | sed "s/licensing_template/$gitdir/g")
 done &&
 
 # rename files 
 FILE_LIST=$(find . -type f -name "*licensing_template*")
 for f in $FILE_LIST
 do 
-    mv $f $(echo $f | sed "s/licensing_template/gitdir/g")
+    mv $f $(echo $f | sed "s/licensing_template/$gitdir/g")
 done &&
 
 # replace all occurrences of "licensing_system" with new system name
-find . -type f -exec sed -i "s/licensing_template/gitdir/g" {} \;
+find . -type f -exec sed -i "s/licensing_template/$gitdir/g" {} \;
 
 # clean licensing-template, copy files to new project then delete licensing-template
 rm -rf .git && rm rename_project.sh &&
-cd ../ && cp -r licensing_template/ test_dir/ &&
+cd ../ && cp -r licensing-template/ test_dir/ &&
 
 echo "Files have been copied to your new repo" &&
 rm -rf licensing-template/
