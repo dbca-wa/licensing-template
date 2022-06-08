e1d708a4ad5eecea2a6c100d9f62ed4094516673
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-08 12:05:42 +0800

    Update rename_project.sh to self-delete

--- rename_project.sh
+++ rename_project.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 
 set -e
-if [[ $# -lt 1 ]]; then
-    echo "Missing script parameter:  you must supply the system name to replace 'licensing_template'"
+#if [[ $# -lt 1 ]]; then
+#    echo "Missing script parameter:  you must supply the system name to replace 'licensing_template'"
+#    echo "$0 1"
+#    exit 1
+#fi
+
+# Gather required user input
+read -p 'Github username: ' gituser
+read -p 'Github repo name: ' gitrepo
+
+gituser=${gituser,,}
+gitdir=${gitrepo/"-"/"_"}
+
+# clone gitrepo
+{
+    cd ../
+    git clone git@github.com:$gituser/$gitrepo.git test_dir
+} ||
+{
+    echo "Error: $gitrepo repo/fork does not exist on your Github account $gituser"
     echo "$0 1"
     exit 1
-fi
+}
+
+# return to licensing-template folder
+cd licensing-template &&
 
 # rename dirs
-mv ./licensing_template/ ./$1/ &&
+mv ./licensing_template/ ./gitdir/ &&
 DIR_LIST=$(find . -type d -name "*licensing_template*")
 for f in $DIR_LIST
 do 
-    mv $f $(echo $f | sed "s/licensing_template/$1/g")
+    mv $f $(echo $f | sed "s/licensing_template/gitdir/g")
 done &&
 
 # rename files 
 FILE_LIST=$(find . -type f -name "*licensing_template*")
 for f in $FILE_LIST
 do 
-    mv $f $(echo $f | sed "s/licensing_template/$1/g")
+    mv $f $(echo $f | sed "s/licensing_template/gitdir/g")
 done &&
 
 # replace all occurrences of "licensing_system" with new system name
-#find . -type f -exec sed -i "s/licensing_template/$1/g" {} \;
-find . -path ./.git -prune -o -print | xargs sed -i "s/licensing_template/$1/g"
+find . -type f -exec sed -i "s/licensing_template/gitdir/g" {} \;
+
+# clean licensing-template, copy files to new project then delete licensing-template
+rm -rf .git && rm rename_project.sh &&
+cd ../ && cp -r licensing_template/ test_dir/ &&
+
+echo "Files have been copied to your new repo" &&
+rm -rf licensing-template/
+echo "Licensing template has been removed"
+
