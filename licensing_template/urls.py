9bdcfae8d15bd1be38364ca23bc5d08abbaf2140
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2022-06-25 07:30:55 +0800

    Add django_media_serv

--- licensing_template/urls.py
+++ licensing_template/urls.py
@@ -70,8 +70,7 @@ urlpatterns = [
 ] + ledger_patterns
 
 
-if settings.DEBUG:  # Serve media locally in development.
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not are_migrations_running():
     DefaultDataManager()
